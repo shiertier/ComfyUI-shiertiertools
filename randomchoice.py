6d6e093c4cf97ca1cb42a23a8d17f8659a929bf7
shiertier <junjie.text@gmail.com>
2024-12-03 00:24:53 +0800

    update

--- randomchoice.py
+++ randomchoice.py
@@ -8,7 +8,8 @@ class RandomChoiceList:
     def INPUT_TYPES(s):
         return {
             "required": {
-                "input_list": ("SIMPLE_PROMPT_LIST", ),  # 使用多行字符串输入作为列表
+                "input_list": ("SIMPLE_PROMPT_LIST",),  # 使用多行字符串输入作为列表
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff, "tooltip": "The random seed used for creating the noise."}),  # 添加随机种子输入
             }
         }
     
@@ -16,11 +17,11 @@ class RandomChoiceList:
     FUNCTION = "random_choice"
     CATEGORY = "utils"
     
-    def random_choice(self, input_list: List[str]) -> tuple[str]:
+    def random_choice(self, input_list: List[str], seed: int) -> tuple[str]:
         """从输入列表中随机选择一个元素"""
         if not input_list:
             raise ValueError("输入列表不能为空")
-        
+        print(seed)
         # 随机选择一个元素并返回
         # ComfyUI要求返回tuple，所以用(result,)的形式
         return (random.choice(input_list),)
